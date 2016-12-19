d4c44db26b15b76872587213e68151b46203fda5
kate <neakriss@narod.ru>
2016-12-19 16:14:05 +0300

    Functions

--- Snakelokal/Snakelokal/Program.cs
+++ Snakelokal/Snakelokal/Program.cs
@@ -10,8 +10,24 @@ namespace Snakelokal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            int x1 = 1;
+            int y1 = 3;
+            char symb = '$';
+            Drow(x1, y1, symb);
+
+            int x2 = 4;
+            int y2 = 6;
+            char symb2 = '#';
+            Drow(x2, y2, symb2);
+
+
             Console.ReadLine();
+        }
+
+        static void Drow(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
