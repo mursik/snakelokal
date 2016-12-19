8af81fef22674f1f7b84e47619f435dfe2c03523
kate <neakriss@narod.ru>
2016-12-19 16:53:09 +0300

    classes

---
+++ Snakelokal/Snakelokal/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snakelokal
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+
+        public void Drow()
+        {
+            Console.SetCursorPosition( x, y );
+            Console.Write( symb );
+        }
+    }
+}
--- Snakelokal/Snakelokal/Program.cs
+++ Snakelokal/Snakelokal/Program.cs
@@ -10,24 +10,21 @@ namespace Snakelokal
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char symb = '$';
-            Drow(x1, y1, symb);
+            Point p1 = new Point();
 
-            int x2 = 4;
-            int y2 = 6;
-            char symb2 = '#';
-            Drow(x2, y2, symb2);
+            p1.x = 1;
+            p1.y = 3;
+            p1.symb = '0';
+            p1.Drow();
 
+            Point p2 = new Point();
+
+            p2.x = 4;
+            p2.y = 5;
+            p2.symb = '#';
+            p2.Drow();
 
             Console.ReadLine();
-        }
-
-        static void Drow(int x, int y, char symb)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(symb);
         }
     }
 }
